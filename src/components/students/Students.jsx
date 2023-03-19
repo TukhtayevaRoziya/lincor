83472b2c89bda97c830455f315bbfda235e96c9a
TukhtayevaRoziya <muhammadumarmuhammadzoda@gmail.com>
2023-03-19 23:00:45 +0500

    student

--- src/components/students/Students.jsx
+++ src/components/students/Students.jsx
@@ -4,7 +4,14 @@ import styles from './Students.module.css'
 const Students = () => {
   return (
     <div className={styles.student}>
-      
+      <h1 className={styles.title}>Yangi o’quvchi qo’shish </h1>
+      <div>
+        <div>
+          <label id='student_name'>O’quvchi ismi</label>
+          <input name=''/>
+        </div>
+        </div>
+
     </div>
   )
 }
