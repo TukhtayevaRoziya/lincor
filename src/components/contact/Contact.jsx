f625db2380614afba8d957ca16b581006f542878
TukhtayevaRoziya <muhammadumarmuhammadzoda@gmail.com>
2023-03-19 18:45:49 +0500

    table done

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,55 +1,87 @@
-import React from 'react'
-import { Table } from 'antd';
-import {AiOutlineDelete} from 'react-icons/ai'
-import styles from './Contact.module.css'
+import React from "react";
+import { Popconfirm } from "antd";
+import { AiOutlineDelete } from "react-icons/ai";
+
+import useWindowSize from "./../../utility/hooks";
+import styles from "./Contact.module.css";
 
 const Contact = () => {
+  const { width } = useWindowSize();
 
-  const columns = [
-    {
-      title: '№',
-      dataIndex: 'num',
-    },
-    {
-      title: 'O’quvchi ismi',
-      dataIndex: 'nameOfStudent',
-    },
-    {
-      title: 'Telefon nomer',
-      dataIndex: 'tel',
-    },
-    {
-      title: 'Izoh',
-      dataIndex: 'comment',
-    },
-    {
-      title: ' ',
-      dataIndex: 'action',
-    },
-  ];
-  
   const data = [];
-  for (let i = 0; i < 46; i++) {
+  for (let i = 0; i < 5; i++) {
     data.push({
       key: i,
-      num: i+1,
+      num: i + 1,
       nameOfStudent: `Edward King ${i}`,
-      tel: 32,
+      tel: "+" + 99890011386 + i,
       comment: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Bibendum nascetur faucibus quam nunc mattis. Etiam ultrices vel nisl nisl nec sed.  ${i}`,
-      action: <AiOutlineDelete/>
-    
+      action: (
+        <Popconfirm
+          title="Delete the task"
+          description="Are you sure to delete this task?"
+          okText="Yes"
+          cancelText="No"
+        >
+          <AiOutlineDelete />
+        </Popconfirm>
+      ),
     });
   }
+  const dataMap = data.map(d=><tr key={d.key}>
+    <td>{d.num}</td>
+    <td>{d.nameOfStudent}</td>
+    <td>{d.tel}</td>
+    <td>{d.comment.length > 80 ? d.comment.slice(0, 80) + '...' : d.comment}</td>
+    <td>{d.action}</td>
+  </tr>)
+
+const myWidth = width < 992 ? width - 120  :  width < 1200 ?  width - 350 : null
 
   return (
-    <div>
+    <>
+    <div className={styles.contact}>
       <h1 className={styles.title}>Bugungi murojatlar</h1>
-      <div className={styles.table_wrap}>
-
-      <Table columns={columns} dataSource={data} />
+      <div className={styles.table_wrap} style={{width: `${myWidth}px`}}>
+        <table className={styles.table}>
+          <thead>
+            <tr>
+              <th>№</th>
+              <th>O’quvchi ismi</th>
+              <th>Telefon nomer</th>
+              <th>Izoh</th>
+              <th> </th>
+            </tr>
+          </thead>
+          <tbody>
+            {dataMap}
+          </tbody>
+        </table>
+        {/* <Table className={styles.table} columns={columns} dataSource={data} /> */}
       </div>
     </div>
-  )
-}
+    <div className={styles.contact + ' ' + styles.contact2}>
+      <h1 className={styles.title}>Kechagi murojatlar</h1>
+      <div className={styles.table_wrap} style={{width: `${myWidth}px`}}>
+        <table className={styles.table}>
+          <thead>
+            <tr>
+              <th>№</th>
+              <th>O’quvchi ismi</th>
+              <th>Telefon nomer</th>
+              <th>Izoh</th>
+              <th> </th>
+            </tr>
+          </thead>
+          <tbody>
+            {dataMap}
+          </tbody>
+        </table>
+        {/* <Table className={styles.table} columns={columns} dataSource={data} /> */}
+      </div>
+    </div>
+    </>
+  );
+};
 
-export default Contact
+export default Contact;
